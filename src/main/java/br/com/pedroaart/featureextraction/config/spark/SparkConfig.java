024f9e09138b37e9c49015959c0ee1719755d3a8
pedroaart <pedroarthurfreitasdias@gmail.com>
2025-08-05 21:09:19 -0300

    Update SparkConfig.java

--- src/main/java/br/com/pedroaart/featureextraction/config/spark/SparkConfig.java
+++ src/main/java/br/com/pedroaart/featureextraction/config/spark/SparkConfig.java
@@ -13,11 +13,7 @@ public class SparkConfig {
     public SparkSession sparkSession() {
         return SparkSession.builder()
                 .appName("MobilityAnalysisApp")
-                .config("spark.mongodb.read.connection.uri",
-                        "mongodb+srv://pedroaart:Pimentas1%23@feature-extraction.x2phx.mongodb.net/feature-extraction.measurements?retryWrites=true&w=majority&appName=feature-extraction")
-                .config("spark.mongodb.write.connection.uri",
-                        "mongodb+srv://pedroaart:Pimentas1%23@feature-extraction.x2phx.mongodb.net/feature-extraction.mobility_analysis?retryWrites=true&w=majority&appName=feature-extraction")
-                .config("spark.sql.adaptive.enabled", "true")
+              .config("spark.sql.adaptive.enabled", "true")
                 .master("local[*]")
                 .getOrCreate();
     }
